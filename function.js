512a5451ebd283532916474ceb9db8d1d5c50801
Larissa Kim <kimLarissa04@gmail.com>
2023-09-09 17:13:54 -0500

    line 34 in html file - test marker

--- function.js
+++ function.js
@@ -5,3 +5,15 @@ const fs = require("fs");
 const { parse } = require("csv-parse");
 // replace example with actual csv file name
 fs.createReadStream("./example.csv"); 
+fs.createReadStream("./example.csv")
+  .pipe(parse({ delimiter: ",", columns: true, ltrim: true }))
+  .on("data", function (row) {
+    console.log(row);
+    bus_marker_info.push(row)
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  })
+  .on("end", function () {
+    console.log("finished");
+  });
