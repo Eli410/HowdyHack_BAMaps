877a97558bc986a0e9bba08ede28e6d4c394666c
Larissa Kim <kimLarissa04@gmail.com>
2023-09-09 17:12:26 -0500

    gotta replace "example" with actualy csv file name. but we can now read in csv files containing bus info.

--- function.js
+++ function.js
@@ -1,1 +1,7 @@
+// jagged array - name of stop, bus#, lat, long
+let bus_marker_info = []
 
+const fs = require("fs");
+const { parse } = require("csv-parse");
+// replace example with actual csv file name
+fs.createReadStream("./example.csv"); 
