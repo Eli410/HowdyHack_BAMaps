ce5404b156b0292928c91f90cb8059ac5564d7a0
Eli410 <mcqueen0410@hotmail.com>
2023-09-10 01:23:58 -0500

    commited Gio's code

--- function.js
+++ function.js
@@ -1,61 +1,46 @@
-function readCsv(filename){
-  const fs = require('fs');
-  let list=[];
-  try {
-    const fileData = fs.readFileSync(filename, 'utf8');
-    const rows = fileData.split('\n');
-    
-    for (let i = 1; i < rows.length; i++) {
-      const elements=rows[i].split(',')
-      list.push(elements)
-    }
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      console.error(`The file '${filename}' was not found.`);
-    } else {
-      console.error(`An error occurred: ${error.message}`);
-    }
-  }
-
-  return list;
-}
-
-
-
 function findClosest(inCoord, coordList) {
-  let minDistance = Infinity;
-  let closestPoint = null;
-
-  for (let coord of coordList) {
-      const distance = Math.sqrt(Math.pow(inCoord[0] - coord[0], 2) + Math.pow(inCoord[1] - coord[1], 2));
-      if (distance < minDistance) {
-          minDistance = distance;
-          closestPoint = coord;
-      }
-  }
-
-  return closestPoint;
-}
-
-function getClostestStop(inputCoords) {
-  const busStops=readCsv('stops.csv')
-
-  let list=[]
-
-  for(let row of busStops){
-    list.push([Number(row[1]),Number(row[2])])
-  }
-
-  return findClosest(inputCoords, list)
-}
-
-function findIntendedStop(id_num){
-  const coord_list=readCsv('stops.csv')
-  for(let i = 1; i < coord_list.length;i++){
-    if(coord_list[i][0] == id_num){
-      return coord_list[i];
-    
+    let minDistance = Infinity;
+    let closestPoint = null;
+  
+    for (let coord of coordList) {
+        const distance = Math.sqrt(Math.pow(inCoord[0] - coord[1], 2) + Math.pow(inCoord[1] - coord[2], 2));
+        if (distance < minDistance) {
+            minDistance = distance;
+            closestPoint = coord;
+        }
     }
+  
+    return closestPoint;
   }
-  return null;
-}
+
+
+
+  const fs = require('fs');
+  
+  function getClostestStop(inputCoords) {
+    const filePath='stops.csv';
+    let list=[];
+    let rows=null;
+    try {
+      const fileData = fs.readFileSync(filePath, 'utf8');
+      rows = fileData.split('\n');
+  
+      for (let i = 1; i < rows.length; i++) {
+        const element=rows[i].split(',')
+        list.push(element)
+      }
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.error(`The file '${filePath}' was not found.`);
+      } else {
+        console.error(`An error occurred: ${error.message}`);
+      }
+    }
+  
+    return findClosest(inputCoords, list);
+  }
+  
+  
+  // Example usage:
+  const filePath = 'stops.csv'; // Replace with the path to your CSV file
+  console.log(getClostestStop([30.565267, -96.296772]));
